c8cef0f4d05049ff2022c0a4622001de3c79e807
Matthew Knopp <mknopp@yammer-inc.com>
2010-01-31 22:55:37 -0800

    initial experiment: ruby-hmac api that uses openssl for digest and hmac functions

---
+++ lib/hmac.rb
@@ -0,0 +1,61 @@
+module HMAC
+  class Base < OpenSSL::HMAC
+    def Base.digest(key, text)
+      begin
+        hmac = self.new(key)
+        hmac.update(text)
+        hmac.digest 
+      ensure 
+        hmac.reset
+      end
+    end
+
+    def Base.hexdigest(key, text)
+      begin 
+        hmac = self.new(key)
+        hmac.update(text)
+        hmac.hexdigest
+      ensure 
+        hmac.reset
+      end
+    end 
+  end
+
+  class MD5 < Base
+    def initialize(key)
+      super(key,OpenSSL::Digest::Digest.new('md5'))
+    end
+  end
+
+  class SHA1 < Base
+    def initialize(key)
+      super(key,OpenSSL::Digest::Digest.new('sha1'))
+    end
+  end
+
+  class SHA256 < Base
+    def initialize(key)
+      super(key,OpenSSL::Digest::Digest.new('sha256'))
+    end
+  end
+
+  class SHA384 < Base
+    def initialize(key)
+      super(key,OpenSSL::Digest::Digest.new('sha384'))
+    end
+  end
+
+  class SHA512 < Base
+    def initialize(key)
+      super(key,OpenSSL::Digest::Digest.new('sha512'))
+    end
+  end
+
+  
+  class RMD160 < Base
+    def initialize(key)
+      super(key,OpenSSL::Digest::Digest.new('rmd160'))
+    end
+  end
+
+end
